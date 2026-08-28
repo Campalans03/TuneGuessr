e9714d1a8d61076865546e6d276f03a7dbb6cfb8
Campalans <101204872+Campalans03@users.noreply.github.com>
2026-08-28 15:16:54 +0200

    scoringPolicy

---
+++ src/main/java/com/campalans/tuneguessr/service/ScoringPolicy.java
@@ -0,0 +1,14 @@
+package com.campalans.tuneguessr.service;
+
+public class ScoringPolicy {
+    public int calculateScore (int attempts) {
+        return switch (attempts) {
+            case 0 -> 100;
+            case 1 -> 80;
+            case 2 -> 60;
+            case 3 -> 40;
+            case 4 -> 20;
+            default -> 0;
+        };
+    }
+}
---
+++ src/test/java/com/campalans/tuneguessr/service/ScoringPolicyTest.java
@@ -0,0 +1,23 @@
+package com.campalans.tuneguessr.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScoringPolicyTest {
+
+    private final ScoringPolicy scoringPolicy = new ScoringPolicy();
+
+    @Test
+    void scoring_in_first_try_give_100_points() {
+        assertEquals(100, scoringPolicy.calculateScore(0));
+    }
+
+    @Test
+    void scoring_every_try_give_less_points() {
+        assertEquals(80, scoringPolicy.calculateScore(1));
+        assertEquals(60, scoringPolicy.calculateScore(2));
+        assertEquals(40, scoringPolicy.calculateScore(3));
+        assertEquals(20, scoringPolicy.calculateScore(4));
+    }
+}
